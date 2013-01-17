320540159dd1f9c0e5cfe41588145662cdb00c54
Alex Willisson <atw@mit.edu>
2013-01-17 04:50:12 -0500

    example linking works

--- demo.c
+++ demo.c
@@ -9,12 +9,30 @@
 #define BACKGROUND 0xffffff
 #define FOREGROUND 0x000000
 #define FONT "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono.ttf"
+#define RED(c) (((c) >> 16) && 0xff)
+#define GREEN(c) (((c) >> 8) & 0xff)
+#define BLUE(c) ((c) && 0xff)
 
-SDL_Surface *screen, *imgTxt;
-SDL_Rect txtRect;
-SDL_Color fColor;
+struct pt {
+	int x, y;
+};
+
+struct icon {
+	SDL_Rect rect;
+	SDL_Surface *text;
+	struct pt center;
+	int x1, y1, x2, y2;
+};
+
+SDL_Surface *screen;
+SDL_Color font_color;
 
 TTF_Font *font;
+
+struct icon icons[100];
+
+int dragging, linked, mousebutton[10];
+struct pt mouse, start;
 
 void process_input (void);
 void draw (void);
@@ -33,7 +51,47 @@ process_input (void)
 		case SDL_KEYUP:
 			if (key == SDLK_ESCAPE || key == 'q') {
 				exit (0);
+			} else if (key == 'r') {
+				linked = 0;
+				dragging = 0;
 			}
+			break;
+		case SDL_MOUSEMOTION:
+			mouse.x = event.button.x;
+			mouse.y = event.button.y;
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			if (event.button.button < 0
+			    || event.button.button >= 10)
+				break;
+
+			mousebutton[event.button.button] = 1;
+
+			if (mouse.x >= icons[0].x1
+			    && mouse.x <= icons[0].x2
+			    && mouse.y >= icons[0].y1
+			    && mouse.y <= icons[0].y2) {
+				dragging = 1;
+				start = mouse;
+			}
+
+			break;
+		case SDL_MOUSEBUTTONUP:
+			if (event.button.button < 0
+			    || event.button.button >= 10)
+				break;
+
+			mousebutton[event.button.button] = 0;
+
+			if (mouse.x >= icons[1].x1
+			    && mouse.x <= icons[1].x2
+			    && mouse.y >= icons[1].y1
+			    && mouse.y <= icons[1].y2) {
+				dragging = 0;
+				linked = 1;
+			}
+
+			dragging = 0;
 			break;
 		}
 	}
@@ -42,7 +100,25 @@ process_input (void)
 void
 draw (void)
 {
-	SDL_BlitSurface (imgTxt, NULL, screen, &txtRect);
+	SDL_BlitSurface (icons[0].text, NULL, screen, &icons[0].rect);
+	SDL_BlitSurface (icons[1].text, NULL, screen, &icons[1].rect);
+
+	roundedRectangleRGBA (screen, icons[0].x1, icons[0].y1,
+			      icons[0].x2, icons[0].y2,
+			      4, 0x00, 0x00, 0x00, 0xff);
+
+	roundedRectangleRGBA (screen, icons[1].x1, icons[1].y1,
+			      icons[1].x2, icons[1].y2,
+			      4, 0x00, 0x00, 0x00, 0xff);
+
+	if (dragging)
+		aalineRGBA (screen, start.x, start.y, mouse.x, mouse.y,
+			    0x88, 0x88, 0x88, 0xff);
+
+	if (linked)
+		aalineRGBA (screen, icons[0].x2, icons[0].center.y,
+			    icons[1].x1, icons[1].center.y,
+			    0x88, 0x88, 0x88, 0xff);
 }
 
 int
@@ -64,12 +140,29 @@ main (int argc, char **argv)
 	TTF_Init ();
 	font = TTF_OpenFont (FONT, 24);
 
-	txtRect.x = 10;
-	txtRect.y = 150;
-	fColor.r = 0x00;
-	fColor.g = 0x00;
-	fColor.b = 0x00;
-	imgTxt = TTF_RenderText_Blended (font, "hello, world", fColor);
+	font_color.r = RED (FOREGROUND);
+	font_color.g = GREEN (FOREGROUND);
+	font_color.b = BLUE (FOREGROUND);
+
+	icons[0].rect.x = 50;
+	icons[0].rect.y = 50;
+	icons[0].text = TTF_RenderText_Blended (font, "Input", font_color);
+	icons[0].x1 = icons[0].rect.x - 5;
+	icons[0].y1 = icons[0].rect.y - 5;
+	icons[0].x2 = icons[0].rect.x + icons[0].text->w + 5;
+	icons[0].y2 = icons[0].rect.y + icons[0].text->h + 5;
+	icons[0].center.x = (icons[0].x1 + icons[0].x2) / 2;
+	icons[0].center.y = (icons[0].y1 + icons[0].y2) / 2;
+
+	icons[1].rect.x = 250;
+	icons[1].rect.y = 50;
+	icons[1].text = TTF_RenderText_Blended (font, "Output", font_color);
+	icons[1].x1 = icons[1].rect.x - 5;
+	icons[1].y1 = icons[1].rect.y - 5;
+	icons[1].x2 = icons[1].rect.x + icons[1].text->w + 5;
+	icons[1].y2 = icons[1].rect.y + icons[1].text->h + 5;
+	icons[1].center.x = (icons[1].x1 + icons[1].x2) / 2;
+	icons[1].center.y = (icons[1].y1 + icons[1].y2) / 2;
 
 	while (1) {
 		process_input ();
